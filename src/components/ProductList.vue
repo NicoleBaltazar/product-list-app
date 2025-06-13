90d1bc80e97a70f12887b528c9eaabc23f96479e
nics-simplevia <nicole.baltazar@simplevia.com>
2025-06-13 15:07:29 +0800

    some images not displaying - fixed

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -21,8 +21,7 @@
       >
         <div class="card h-100 shadow-sm">
           <img
-            :src="product.images[0]"
-            referrerpolicy="no-referrer"
+            :src="product.images[1]"
             class="card-img-top object-fit-cover"
             alt="Product Image"
             style="height: 200px"
